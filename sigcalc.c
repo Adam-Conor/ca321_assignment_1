e92e183f26a4394ba437efa383648f5f84da057d
Conor Smyth <cnrsmyth@gmail.com>
2014-10-18 01:23:04 +0100

    added functionality+calc thread. Still needs work.

--- sigcalc.c
+++ sigcalc.c
@@ -2,18 +2,25 @@
 #include <pthread.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdbool.h>
+
+int x;
+int y;
+bool endOfFile;
+sigset_t set;
+int sig;
 
 static void *
 reader(void *in) {
 	char** argv = in;	
 	FILE *fp; //var for file
-	int x; //vars for numbers to add
-	int y;
+	//int x; //vars for numbers to add
+	//int y;
 	//sigset_t set;
 	//int sig;
 
-	//sigemptyset(&set);
-	//sigaddset(&set, SIGUSR1);
+	sigemptyset(&set);
+	sigaddset(&set, SIGUSR1);
 
 	fp = fopen(argv[1], "r"); //open file
 
@@ -26,25 +33,54 @@ reader(void *in) {
 		fscanf(fp, "%d %d", &x, &y); //get two ints
 
 		if(feof(fp)) { //check for end of file
+			endOfFile = true;
 			break;
 		}
 
 		printf("%d %d\n", x, y); //print out the ints
-		
-		//sigwait(&set, &sig);
+		/** TODO **/
+		//signal to calc
+		//sig
+		usleep(1000);
+
+		/*signal(SIGUSR1, &calculator);
+		sigwait(&set, &sig);
+		sleep(1);
+		*/
 	}
 
 	fclose(fp); //close the file
 	return((void *)NULL);
 }
 
+static void *
+calculator(void *in) {
+	//sigemptyset(&set);
+	//sigaddset(&set, SIGUSR1);
+
+	while(!endOfFile) {
+		/** TODO **/
+		//wait for signal
+		//sigwait(&set, &sig);
+		printf("%d\n", x + y);
+		//signal(SIGUSR1, &reader);
+		/** TODO **/
+		//signal reader
+		usleep(1000);
+	}
+	return((void *)NULL);
+}
+
 
 int main(int arc, char *argv[]) {
-	pthread_t read;
+	pthread_t read; //init threads
+	pthread_t calc;
 
-	pthread_create(&read, NULL, &reader, (void *)argv);
+	pthread_create(&read, NULL, &reader, (void *)argv); //create threads
+	pthread_create(&calc, NULL, &calculator, (void *)NULL);
 
-	pthread_join(read, NULL);
+	pthread_join(read, NULL); //starts threads
+	pthread_join(calc, NULL);
 
 	return(0);
 }
