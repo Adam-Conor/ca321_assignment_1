23655cb0f488cede15a7e77e117665ad03afb983
Conor Smyth <cnrsmyth@gmail.com>
2014-10-26 00:21:44 +0100

    Fixed up file. Need to add signals

--- sigcalc.c
+++ sigcalc.c
@@ -4,20 +4,17 @@
 #include <unistd.h>
 #include <stdbool.h>
 
+#define SLEEP 10000
+
 int x;
 int y;
 bool endOfFile;
 sigset_t set;
 int sig;
 
-static void *
-reader(void *in) {
+static void * reader(void *in) {
 	char** argv = in;	
 	FILE *fp; //var for file
-	//int x; //vars for numbers to add
-	//int y;
-	//sigset_t set;
-	//int sig;
 
 	sigemptyset(&set);
 	sigaddset(&set, SIGUSR1);
@@ -29,58 +26,48 @@ reader(void *in) {
 		return((void *)NULL);
 	}
 
-	while(1) {
+	while(!feof(fp)) {
 		fscanf(fp, "%d %d", &x, &y); //get two ints
 
-		if(feof(fp)) { //check for end of file
-			endOfFile = true;
-			break;
-		}
+		printf("Thread 1 submitting : %d %d\n", x, y); //print out the ints
 
-		printf("%d %d\n", x, y); //print out the ints
-		/** TODO **/
-		//signal to calc
-		//sig
-		usleep(1000);
+		//signal to go here
 
-		/*signal(SIGUSR1, &calculator);
-		sigwait(&set, &sig);
-		sleep(1);
-		*/
+		//sigwait(&set, &sig);
+
+		usleep(SLEEP);
 	}
+
+	endOfFile = true;
 
 	fclose(fp); //close the file
 	return((void *)NULL);
 }
 
-static void *
-calculator(void *in) {
-	//sigemptyset(&set);
-	//sigaddset(&set, SIGUSR1);
-
+static void * calculator(void *in) {
 	while(!endOfFile) {
-		/** TODO **/
-		//wait for signal
 		//sigwait(&set, &sig);
-		printf("%d\n", x + y);
-		//signal(SIGUSR1, &reader);
-		/** TODO **/
-		//signal reader
-		usleep(1000);
+
+		printf("Thread 2 calculated : %d\n", x + y);
+
+		//signal to go here
+
+		usleep(SLEEP);
 	}
+
 	return((void *)NULL);
 }
 
 
 int main(int arc, char *argv[]) {
-	pthread_t read; //init threads
-	pthread_t calc;
+	pthread_t read_t; //init threads
+	pthread_t calc_t;
 
 	pthread_create(&read, NULL, &reader, (void *)argv); //create threads
 	pthread_create(&calc, NULL, &calculator, (void *)NULL);
 
-	pthread_join(read, NULL); //starts threads
-	pthread_join(calc, NULL);
+	pthread_join(read_t, NULL); //starts threads
+	pthread_join(calc_t, NULL);
 
 	return(0);
 }
