b6d4401f188a36c353be0de242f5caf8cea7a926
Conor Smyth <cnrsmyth@gmail.com>
2014-10-30 21:21:06 +0000

    Trying to implement signal. Getting seg fault.

--- sigcalc.c
+++ sigcalc.c
@@ -6,104 +6,112 @@
 
 #define SLEEP 10000
 
-int x;
-int y;
-sigset_t set;
-int sig;
-
 static void * reader(void *in);
 static void * calculator(void *in);
 static void * control(void *in);
 
+//struct to hold data
 typedef struct {
 	int x;
 	int y;
 	FILE *fp;
 } values_t;
 
-static void * reader(void *in) {
-	//char** argv = in;	
-	//FILE *fp; //var for file
+static void * reader(void *val_in) {
+	values_t *val = val_in;
+	sigset_t set;
+	int sig;// = SIGUSR1;
+	
+	//set signal
+	sigemptyset(&set);
+	sigset(SIGUSR1, calculator);
+	//sigsuspend(&set);
+
+	while(!feof(val->fp)) {
+		//read in the two ints
+		fscanf(val->fp, "%d %d", &val->x, &val->y);
+		
+		//print out valuesto be calculated
+		printf("Thread 1 submitting : %d %d\n", val->x, val->y); //print out the ints
+
+		//signal to go here
+		//signal(SIGUSR1, NULL);
+		//wait for signal
+		//alarm(1);
+		signal(SIGUSR1,(void *)calculator);
+		//sigwait(&set, &sig);
+		sigsuspend(&set);
+		//pthread_kill(pthread_self(),SIGUSR1);
+		//sleep
+		usleep(SLEEP);
+	}
+
+	return((void *)NULL);
+}//reader
+
+static void * calculator(void *val_in) {
+	values_t *val = val_in;
 	sigset_t set;
 	int sig;
 
+	//set signal
 	sigemptyset(&set);
-	sigset(SIGUSR1, calculator);
+	sigset(SIGALRM, reader);
 
-	//fp = fopen(argv[1], "r"); //open file
-
-	/*if(fp == NULL) { //check for valid file
-		perror("error opening file");
-		return((void *)NULL);
-	}
-	*/
-
-	while(!feof(fp)) {
-		fscanf(fp, "%d %d", &x, &y); //get two ints
-
-		printf("Thread 1 submitting : %d %d\n", x, y); //print out the ints
-
-		//signal to go here
-		pthread_cond_signal(&sig);
-
-		sigwait(&set, &sig);
-
-		usleep(SLEEP);
-	}
-
-	//fclose(fp); //close the file
-	return((void *)NULL);
-}
-
-static void * calculator(void *in) {
 	while(1) {
+		//wait for signal
+		sigwait(&set, &sig);
+		//sigsuspend(&set);
+		
+		//calculate 
+		printf("Thread 2 calculated : %d\n", val->x + val->y);
 		sigwait(&set, &sig);
 
-		printf("Thread 2 calculated : %d\n", x + y);
-
-		//signal to go here
-		pthread_cond_signal(&sig);
-
+		//signal(SIGUSR1, NULL);
+		//sleep
 		usleep(SLEEP);
 	}
 
 	return((void *)NULL);
-}
+}//calculator
 
 static void * control(void *in) {
 	char** argv = in;
 	values_t val;
 
+	//open file
 	val.fp = fopen(argv[1], "r");
 
-	if(fp == NULL) {
+	if(val.fp == NULL) {
 			perror("error opening file");
 			return((void *)NULL);
-	}
+	}//ensure file valid
 
-	pthread_t read_t; //init threads
+	//init threads
+	pthread_t read_t;
 	pthread_t calc_t;
+	
+	//create threads
+	pthread_create(&read_t, NULL, &reader, (void *)&val);
+	pthread_create(&calc_t, NULL, &reader, (void *)&val);
+	alarm(1);
 
-	pthread_create(&read_t, NULL, &reader, (void *)&val); //create threads
-	pthread_create(&calc, NULL, &reader, (void *)&val); //create threads
+	//start threads
+	pthread_join(read_t, NULL);
+	pthread_join(calc_t, NULL);
 
+	//close file
 	fclose(val.fp);
 	return((void *)NULL);
-}
+}//control
 
 int main(int arc, char *argv[]) {
-	pthread_t cont_t; //init threads
-	//pthread_t read_t; //init threads
-	//pthread_t calc_t;
+	pthread_t cont_t; //init thread
+	signal(SIGALRM, (void *)reader);
 
-	//pthread_create(&read, NULL, &reader, (void *)argv); //create threads
-	//pthread_create(&calc, NULL, &calculator, (void *)NULL);
-	pthread_create(&cont_t, NULL, &reader, (void *)argv); //create threads
+	pthread_create(&cont_t, NULL, &control, (void *)argv); //create thread
 
-	//pthread_join(read_t, NULL); //starts threads
-	//pthread_join(calc_t, NULL);
-	pthread_join(cont_t, NULL);
-	
+	pthread_join(cont_t, NULL); //start thread
 
 	return(0);
 }
