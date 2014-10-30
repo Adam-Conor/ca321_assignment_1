0dbf61a207a7a3111bd9e8cc1142b7960394c774
Conor Smyth <cnrsmyth@gmail.com>
2014-10-30 20:26:01 +0000

    added idea of controller thread and struct

--- sigcalc.c
+++ sigcalc.c
@@ -11,20 +11,32 @@ int y;
 sigset_t set;
 int sig;
 
+static void * reader(void *in);
+static void * calculator(void *in);
+static void * control(void *in);
+
+typedef struct {
+	int x;
+	int y;
+	FILE *fp;
+} values_t;
+
 static void * reader(void *in) {
-	char** argv = in;	
-	FILE *fp; //var for file
-	&sig = SIGUSR1;
+	//char** argv = in;	
+	//FILE *fp; //var for file
+	sigset_t set;
+	int sig;
 
 	sigemptyset(&set);
-	sigaddset(&set, &sig);
+	sigset(SIGUSR1, calculator);
 
-	fp = fopen(argv[1], "r"); //open file
+	//fp = fopen(argv[1], "r"); //open file
 
-	if(fp == NULL) { //check for valid file
+	/*if(fp == NULL) { //check for valid file
 		perror("error opening file");
 		return((void *)NULL);
 	}
+	*/
 
 	while(!feof(fp)) {
 		fscanf(fp, "%d %d", &x, &y); //get two ints
@@ -39,7 +51,7 @@ static void * reader(void *in) {
 		usleep(SLEEP);
 	}
 
-	fclose(fp); //close the file
+	//fclose(fp); //close the file
 	return((void *)NULL);
 }
 
@@ -58,16 +70,40 @@ static void * calculator(void *in) {
 	return((void *)NULL);
 }
 
+static void * control(void *in) {
+	char** argv = in;
+	values_t val;
 
-int main(int arc, char *argv[]) {
+	val.fp = fopen(argv[1], "r");
+
+	if(fp == NULL) {
+			perror("error opening file");
+			return((void *)NULL);
+	}
+
 	pthread_t read_t; //init threads
 	pthread_t calc_t;
 
-	pthread_create(&read, NULL, &reader, (void *)argv); //create threads
-	pthread_create(&calc, NULL, &calculator, (void *)NULL);
+	pthread_create(&read_t, NULL, &reader, (void *)&val); //create threads
+	pthread_create(&calc, NULL, &reader, (void *)&val); //create threads
 
-	pthread_join(read_t, NULL); //starts threads
-	pthread_join(calc_t, NULL);
+	fclose(val.fp);
+	return((void *)NULL);
+}
+
+int main(int arc, char *argv[]) {
+	pthread_t cont_t; //init threads
+	//pthread_t read_t; //init threads
+	//pthread_t calc_t;
+
+	//pthread_create(&read, NULL, &reader, (void *)argv); //create threads
+	//pthread_create(&calc, NULL, &calculator, (void *)NULL);
+	pthread_create(&cont_t, NULL, &reader, (void *)argv); //create threads
+
+	//pthread_join(read_t, NULL); //starts threads
+	//pthread_join(calc_t, NULL);
+	pthread_join(cont_t, NULL);
+	
 
 	return(0);
 }
